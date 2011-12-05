b6b8e54577862ac89b14fb6e9f008df68b5f73d8
Sokolov Yura <funny.falcon@gmail.com>
2011-12-05 10:32:20 +0400

    fixed querying for AR3.1

--- lib/ar_pg_array/querying_arel.rb
+++ lib/ar_pg_array/querying_arel.rb
@@ -119,51 +119,116 @@ else
   end
 end
 
-module ActiveRecord
-  class PredicateBuilder
-    def build_from_hash(attributes, default_table)
-      predicates = attributes.map do |column, value|
-        table = default_table
+if ActiveRecord::VERSION::STRING < '3.1'
+  module ActiveRecord
+    class PredicateBuilder
+      def build_from_hash(attributes, default_table)
+        predicates = attributes.map do |column, value|
+          table = default_table
 
-        if value.is_a?(Hash)
-          table = Arel::Table.new(column, :engine => @engine)
-          build_from_hash(value, table)
-        else
-          column = column.to_s
-
-          if column.include?('.')
-            table_name, column = column.split('.', 2)
-            table = Arel::Table.new(table_name, :engine => @engine)
-          end
-
-          attribute = table[column] || Arel::Attribute.new(table, column)
-
-          case value
-          when PGArrays::PgAny
-            attribute.ar_any(value)
-          when PGArrays::PgAll
-            attribute.ar_all(value)
-          when PGArrays::PgIncludes
-            attribute.ar_included(value)
-          when Array, ActiveRecord::Associations::AssociationCollection, ActiveRecord::Relation
-            values = value.to_a.map { |x|
-              x.is_a?(ActiveRecord::Base) ? x.id : x
-            }
-            attribute.in(values)
-          when Range, Arel::Relation
-            attribute.in(value)
-          when ActiveRecord::Base
-            attribute.eq(value.id)
-          when Class
-            # FIXME: I think we need to deprecate this behavior
-            attribute.eq(value.name)
+          if value.is_a?(Hash)
+            table = Arel::Table.new(column, :engine => @engine)
+            build_from_hash(value, table)
           else
-            attribute.eq(value)
+            column = column.to_s
+
+            if column.include?('.')
+              table_name, column = column.split('.', 2)
+              table = Arel::Table.new(table_name, :engine => @engine)
+            end
+
+            attribute = table[column] || Arel::Attribute.new(table, column)
+
+            case value
+            when PGArrays::PgAny
+              attribute.ar_any(value)
+            when PGArrays::PgAll
+              attribute.ar_all(value)
+            when PGArrays::PgIncludes
+              attribute.ar_included(value)
+            when Array, ActiveRecord::Associations::AssociationCollection, ActiveRecord::Relation
+              values = value.to_a.map { |x|
+                x.is_a?(ActiveRecord::Base) ? x.id : x
+              }
+              attribute.in(values)
+            when Range, Arel::Relation
+              attribute.in(value)
+            when ActiveRecord::Base
+              attribute.eq(value.id)
+            when Class
+              # FIXME: I think we need to deprecate this behavior
+              attribute.eq(value.name)
+            else
+              attribute.eq(value)
+            end
           end
         end
-      end
 
-      predicates.flatten
+        predicates.flatten
+      end
+    end
+  end
+else
+  module ActiveRecord
+    class PredicateBuilder
+      def self.build_from_hash(engine, attributes, default_table)
+        predicates = attributes.map do |column, value|
+          table = default_table
+
+          if value.is_a?(Hash)
+            table = Arel::Table.new(column, engine)
+            build_from_hash(engine, value, table)
+          else
+            column = column.to_s
+
+            if column.include?('.')
+              table_name, column = column.split('.', 2)
+              table = Arel::Table.new(table_name, engine)
+            end
+
+            attribute = table[column.to_sym]
+
+            case value
+            when PGArrays::PgAny
+              attribute.ar_any(value)
+            when PGArrays::PgAll
+              attribute.ar_all(value)
+            when PGArrays::PgIncludes
+              attribute.ar_included(value)
+            when ActiveRecord::Relation
+              value = value.select(value.klass.arel_table[value.klass.primary_key]) if value.select_values.empty?
+              attribute.in(value.arel.ast)
+            when Array, ActiveRecord::Associations::CollectionProxy
+              values = value.to_a.map { |x|
+                x.is_a?(ActiveRecord::Base) ? x.id : x
+              }
+
+              if values.include?(nil)
+                values = values.compact
+                if values.empty?
+                  attribute.eq nil
+                else
+                  attribute.in(values.compact).or attribute.eq(nil)
+                end
+              else
+                attribute.in(values)
+              end
+
+            when Range, Arel::Relation
+              attribute.in(value)
+            when ActiveRecord::Base
+              attribute.eq(value.id)
+            when Class
+              # FIXME: I think we need to deprecate this behavior
+              attribute.eq(value.name)
+            else
+              attribute.eq(value)
+            end
+          end
+        end
+
+        predicates.flatten
+      end
     end
   end
 end
