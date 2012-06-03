f7e26dbc9a157b29ac2d019e2e1e8d7fcbaeadac
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-06-03 20:22:01 +0400

    ...

--- lib/ar_pg_array/schema_cacheable.rb
+++ lib/ar_pg_array/schema_cacheable.rb
@@ -1,11 +1,9 @@
-adjust_cached_types = lambda do |atcbd|
-  atcbd << /_array$/
-  def atcbd.include?(val)
-    any?{|type| type === val}
-  end
-end
-if ActiveRecord::VERSION::MAJOR < 3
-  adjust_cached_types.call(ActiveRecord::AttributeMethods::ATTRIBUTE_TYPES_CACHED_BY_DEFAULT)
-else
-  adjust_cached_types.call(ActiveRecord::AttributeMethods::Read::ATTRIBUTE_TYPES_CACHED_BY_DEFAULT)
+ar_am = ActiveRecord::AttributeMethods
+atcbd = ActiveRecord::VERSION::MAJOR < 3 ?
+    ar_am::ATTRIBUTE_TYPES_CACHED_BY_DEFAULT :
+    ar_am::Read::ATTRIBUTE_TYPES_CACHED_BY_DEFAULT
+
+atcbd << /_array$/
+def atcbd.include?(val)
+  any?{|type| type === val}
 end
